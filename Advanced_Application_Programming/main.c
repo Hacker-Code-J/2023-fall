5b4de8f1814376c7a11d11968f758135371a07c6
Ji, Yong-Hyeon <60635415+Hacker-Code-J@users.noreply.github.com>
2023-10-04 21:16:58 +0900

    Update main.c

--- Advanced_Application_Programming/main.c
+++ Advanced_Application_Programming/main.c
@@ -8,7 +8,7 @@
 * 
 * Author(s): Ji Yong-Hyeon, Kim Ye-chan, Moon Ye-chan, Yoo Geun-oh
 * Date Created: 2023-09-21
-* Last Modified: 2023-09-30
+* Last Modified: 2023-10-04
 *
 * Note: This library aims to provide a robust and efficient solution for
 *       performing arithmetic on large integers beyond the limitations of
@@ -18,6 +18,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
 #include<memory.h>
 #include<errno.h>
 
@@ -25,66 +26,66 @@
 
 int main() {
     WORD k;
-    printf("W = 2^%lu\n\n",8*sizeof(k));
-
-    printf("\n *************[xyz]*************\n\n");
+    printf("W = 2^%lu\n",8*sizeof(k));
     
+    clock_t start, end;
+    /*Test
     BINT* x = NULL;
     BINT* y = NULL;
     BINT* z = NULL;
 
-    init_bint(&x, 3);
-    x->val[0] = 0x99aabbcc;
-    x->val[1] = 0x55667788;
-    x->val[2] = 0x11223344;
-    printf("x: ");
-    printHex(x);
+    init_bint(&x, 2);
+    x->val[0] = 0x87654321;
+    x->val[1] = 0x9;
 
     init_bint(&y, 2);
-    y->val[0] = 0xaabbccdd;
-    y->val[1] = 0x33334444; 
-    printf("y: ");
-    printHex(y);
+    y->val[0] = 0x98765432;
+    y->val[1] = 0xfedcba;
+
+    custom_printHex_xy(x,y,2);
     
     z = add_xy(x,y);
 
     printf("\n\nResult:\n\n");
-    printHex(x);
-    printHex(y);
-    printHex(z);
+    custom_printHex_xyz(x,y,z,2);
 
     delete_bint(&x);
     delete_bint(&y);
     delete_bint(&z);
+    */
 
-    printf("\n *************[abc]*************\n\n");
-    
+
     BINT* a = NULL;
     BINT* b = NULL;
     BINT* c = NULL;
+    int n, m, max;
+    for(int i=0; i<3; i++) {
+        n = rand() % 0x3;
+        m = rand() % 0x3;
+        max = (n>m)? n:m;
 
-    init_bint(&a, 2);
-    a->val[0] = 0xffffffff;
-    a->val[1] = 0xffffffff;
-    printf("a: ");
-    printHex(a);
+        start = clock();
+        printf("\n/****************************\n[Test %d]\n****************************/\n", i+1);
+        rand_bint(&a, NON_NEGATIVE, n);
+        rand_bint(&b, NON_NEGATIVE, m);
 
-    init_bint(&b, 2);
-    b->val[0] = 0xffffffff; 
-    b->val[1] = 0xffffffff; 
-    printf("b: ");
-    printHex(b);
-    
-    c = add_xy(a,b);
+        custom_printHex_xy(a,b,max);
 
-    printf("\n\nResult:\n\n");
-    printHex(a);
-    printHex(b);
-    printHex(c);
+        c = add_xy(a,b);
 
-    delete_bint(&a);
-    delete_bint(&b);
-    delete_bint(&c);
+        printf("\nResult:\n\n");
+        
+        custom_printHex_xyz(a,b,c,max);
+        
+        delete_bint(&a);
+        delete_bint(&b);
+        delete_bint(&c);
+
+        end = clock();
+
+        double time_taken = ((double) end - start) / CLOCKS_PER_SEC;
+        printf("\nTime: %f.\n", time_taken);
+    }
     
     return 0;
 }
