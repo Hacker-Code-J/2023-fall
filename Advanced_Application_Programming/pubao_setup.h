91712c896e9c690bd467bd4150041ce86e11be9e
Ji, Yong-Hyeon <60635415+Hacker-Code-J@users.noreply.github.com>
2023-10-04 21:16:27 +0900

    Update pubao_setup.h

--- Advanced_Application_Programming/pubao_setup.h
+++ Advanced_Application_Programming/pubao_setup.h
@@ -34,8 +34,6 @@ typedef struct {
 } BINT; //16-byte, i.e., 128-bit (32+32+64)
 
 /*4. Error*/
-#define OK 0
-#define Err_Overflow 1
 
 /*5. Basic Funtions*/
 /**********************************************************************
@@ -88,12 +86,61 @@ void delete_bint(BINT** bint_ptr) {
     }
 }
 
-
 void printHex(BINT* X) {
     printf("0x ");
-    for (int i = X->wordlen - 1; i >= 0; i--) {
+    for (int i=X->wordlen-1; i>=0; i--)
         printf("%08x ", X->val[i]);
-    }
+    printf("\n");
+}
+
+void custom_printHex_xyz(BINT* X, BINT* Y, BINT* Z, int n) {
+    //print X 
+    printf("0x ");
+    for(int i=0; i<(n-X->wordlen); i++)
+        printf("%08x ", 0);
+    for (int i=X->wordlen-1; i>=0; i--)
+        printf("%08x ", X->val[i]);
+    printf("\n");
+
+    //print Y
+    printf("0x ");
+    for(int i=0; i<(n-Y->wordlen); i++)
+        printf("%08x ", 0);
+    for (int i=Y->wordlen-1; i>=0; i--)
+        printf("%08x ", Y->val[i]);
+    printf("\n");
+
+    //print line
+    printf("--");
+    for(int i=0; i<n; i++) {
+        printf("-");
+        printf("--------");
+    } printf("\n");
+
+    //print Z
+    printf("0x ");
+    for(int i=0; i<n-Z->wordlen; i++)
+        printf("%08x ", 0);
+    for (int i=Z->wordlen-1; i>=0; i--)
+        printf("%08x ", Z->val[i]);
+    printf("\n");
+}
+
+void custom_printHex_xy(BINT* X, BINT* Y, int n) {
+    //print X 
+    printf("x: 0x ");
+    for(int i=0; i<(n-X->wordlen); i++)
+        printf("%08x ", 0);
+    for (int i=X->wordlen-1; i>=0; i--)
+        printf("%08x ", X->val[i]);
+    printf("\n");
+
+    //print Y
+    printf("y: 0x ");
+    for(int i=0; i<(n-Y->wordlen); i++)
+        printf("%08x ", 0);
+    for (int i=Y->wordlen-1; i>=0; i--)
+        printf("%08x ", Y->val[i]);
     printf("\n");
 }
 
@@ -115,67 +162,95 @@ void refine_BINT(BINT* X) {
         X->sign = NON_NEGATIVE;
 }
 
+//Generate Random BINT
+void rand_bint(BINT**x, int sign, int wordlen) {
+    init_bint(x, wordlen);
+    (*x)->sign = sign;
+    rand_array((*x)->val, wordlen);
+
+    refine_BINT(*x);
+}
+void rand_array(WORD* dst, int wordlen) {
+    u8* p = (u8*)dst; //u8 = byte
+    int cnt = wordlen * sizeof(WORD);
+    while(cnt>0) {
+        *p = rand() & 0xFF;
+        p++;
+        cnt--;
+    }
+}
+
+
 /* 6. Addition and Subtraction */
+void add_xyk(WORD x, WORD y, WORD k, WORD* res, WORD* carry) {
+    /****************
+    *carry = 0;
+    *res = x+y;
+    if(*res < x)
+        *carry = 1;
 
-typedef struct {
-    WORD sum;
-    WORD carry;
-} SumAndCarry;
+    *res = *res + k;
+    if(*res < k)
+        *carry += 1;
+    ****************/
 
-SumAndCarry add_xyk(WORD x, WORD y, WORD k) {
-    SumAndCarry sc;
-    unsigned long long res = (unsigned long long) x + y + k;  // casting to prevent overflow
-    sc.sum = (WORD) (res & 0xFFFFFFFF); // assuming 32-bit words, adjust mask as needed
-    sc.carry = (WORD) (res >> 32);
-    printf("%08x + %08x = %08x * W + %08x\n", x, y, sc.carry, sc.sum);
-    return sc;
+    //Optimize
+    *res = x + y;
+    *carry = (*res < x);
+    *res += k;
+    *carry += (*res < (x+y));
+    printf("%08x + %08x = %08x * W + %08x\n", x, y, *carry, *res);
+}
+void add_xyk2(WORD x, WORD y, WORD k, WORD* res, WORD* carry) {
+    u64 sum = (u64) x + y + k;  // casting to prevent overflow
+    *res = (WORD) (sum & MASK); // assuming 32-bit words, adjust mask as needed
+    *carry = (WORD) (sum >> 32);
+    printf("%08x + %08x = %08x * W + %08x\n", x, y, *carry, *res);
 }
 
 BINT* add_xy(BINT* X, BINT* Y) {
     int n = X->wordlen;
     int m = Y->wordlen;
 
-    // Allocate space for Z = X + Y
-    BINT* Z = malloc(sizeof(BINT));
-    if (Z == NULL) {
+    // Allocate space for Z = X + Y;
+    /*
+    BINT* Z = (BINT*)malloc(sizeof(BINT));
+    if (!Z) { //Z(BINT* == NULL
         perror("Unable to allocate memory");
         exit(1);
     }
-    Z->val = malloc((n + 1) * sizeof(WORD)); // +1 for possible carry
-    if (Z->val == NULL) {
+    Z->val = (WORD*)calloc(n + 1,sizeof(WORD)); // +1 for possible carry
+    if (!Z->val) {
         perror("Unable to allocate memory");
         free(Z);
         exit(1);
+    }*/
+
+    BINT* Z = NULL;
+    init_bint(&Z, n+1);
+
+    WORD k = 0;
+    WORD res, carry;
+
+    // Loop until the shorter of the two numbers ends
+    int i;
+    for (i=0; i<m; i++) {
+        add_xyk(X->val[i], Y->val[i], k, &res, &carry);
+        Z->val[i] = res;
+        k = carry;
+    }
+    // Continue adding any remaining X values with the carry, since Y is shorter
+    for (; i<n; i++) {
+        add_xyk(X->val[i], 0, k, &res, &carry); // just adding X values since Y is shorter
+        Z->val[i] = res;
+        k = carry;
     }
 
-    // Extend Y.val with zeros to make Y and X the same length
-    WORD* tmp_val = NULL;
-    tmp_val = (WORD*)realloc(Y->val, sizeof(WORD)*n);
-    if (!tmp_val) {
-        perror("Unable to allocate memory");
-        exit(1);
-    } else {
-        Y->val = tmp_val;
-    }
-    for (int i=m; i<n; i++) {
-        Y->val[i] = (WORD)0;
-    }
-
-    WORD k = (WORD)0;
-    for (int i = 0; i < n; i++) {
-        SumAndCarry sc = add_xyk(X->val[i], Y->val[i], k);
-        Z->val[i] = sc.sum;
-        k = sc.carry;
-    }
     Z->val[n] = k;
 
     // Set the sign and word length of Z
     Z->sign = X->sign; // assuming the same sign for X and Y
-    Z->wordlen = (k == 0) ? X->wordlen : X->wordlen + 1;
-
-    // Free or reset the memory for Y->val if needed
-    // free(Y->val); // if needed
-    // Y->wordlen = original_wordlen; // if needed
+    Z->wordlen = (k == 0) ? n : n+1;
 
     return Z;
 }
